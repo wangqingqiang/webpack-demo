a954481122cf39ac57ff1e0f5588bdb6ce6b3a5a
qingqiang.wang <qingqiang.wang@qq.com>
2019-07-08 23:27:12 +0800

    初始化

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './assets/style/index.css';
-import './assets/style/main.less';
-import image from './assets/images/main.jpg';
-
-console.log(image);
-console.log('hello,webpack');
-document.body.style.background = `url(${image})`;
